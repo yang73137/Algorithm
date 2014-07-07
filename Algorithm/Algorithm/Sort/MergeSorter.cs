a2a50793adc7e8ba59f87a9ab7851a84072b9399
yang73137 <yang73137@126.com>
2014-07-07 09:05:14 +0800

    Add MegerSorter

---
+++ Algorithm/Algorithm/Sort/MergeSorter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithm.Sort
+{
+    public class MergeSorter : SorterBase
+    {
+        protected override void SortCore<T>(T[] source)
+        {
+        }
+
+    }
+}
---
+++ Algorithm/Algorithm/Struct/BinaryTree.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithm.Struct
+{
+    public class BinaryTree<T>
+    {
+        
+    }
+
+    public class BinaryTreeNode<T>
+    {
+        public T Value { get; set; }
+
+        public BinaryTreeNode<T> Parent { get; set; }
+
+        public BinaryTreeNode<T> LeftChild { get; set; }
+
+        public BinaryTreeNode<T> RightChild { get; set; }
+
+        public bool IsRoot { get; set; }
+
+        public bool IsLeaf { get; set; }
+
+        public BinaryTreeNode()
+        {
+        }
+
+        public BinaryTreeNode(T value)
+        {
+            this.Value = value;
+        }
+
+        public BinaryTreeNode(T value, BinaryTreeNode<T> parent)
+        {
+            this.Value = value;
+            this.Parent = parent;
+        }
+
+        public BinaryTreeNode(T value, BinaryTreeNode<T> parent, BinaryTreeNode<T> leftChild, BinaryTreeNode<T> rightChild)
+        {
+            this.Value = value;
+            this.Parent = parent;
+            this.LeftChild = leftChild;
+            this.RightChild = rightChild;
+        }
+
+        public override string ToString()
+        {
+            return this.Value.ToString();
+        }
+    }
+}
